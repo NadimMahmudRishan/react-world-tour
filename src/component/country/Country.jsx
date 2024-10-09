51ec224990a7d997ba468b1d5ca92a898d3eeee2
Rishan <98330109+NadimMahmudRishan@users.noreply.github.com>
2024-10-10 01:59:36 +0600

    wanna go button word properly.

--- src/component/country/Country.jsx
+++ src/component/country/Country.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import "./country.css";
 
 const Country = ({ country }) => {
   const { name, flags, population, area, cca3 } = country;
+  const [visited, setVisited] = useState(false);
+  const handelVisited = () => {
+    setVisited(!visited);
+  };
   return (
-    <div className="Box">
+    <div className={`Box ${visited && "visited"}`}>
       <h4>Country Name : {name?.common}</h4>
       <img src={flags.png} alt="Flag.png" />
       <p>Population : {population}</p>
       <p>Area : {area}</p>
-      <p><small>CCA3 Code : {cca3}</small></p>
-      <button>Visited</button>
+      <p>
+        <small>CCA3 Code : {cca3}</small>
+      </p>
+      <button onClick={handelVisited}>
+        {visited ? "Visited" : "Wanna Go"}
+      </button>
+      {visited
+        ? "I have visited this country."
+        : "I want to visite this country."}
     </div>
   );
 };
